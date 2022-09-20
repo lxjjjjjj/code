69e56e677dcf0a941f46d9dc776df977bd3cab0f
sarahliu <sarahliu@didiglobal.com>
2022-09-20 11:08:14 +0800

    第一天

---
+++ 括号匹配.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function (s) {
+    if (s.length % 2 === 1) {//如果是奇数肯定false
+        return false;
+    }
+    const stack = [];
+    for (let i = 0; i < s.length; i += 1) {
+        //遍历字符串,并且赋值给c
+        const c = s[i];
+        if (c === '{' || c === '(' || c === '[') {
+            //只要是左括号就入栈
+            stack.push(c);
+        }
+        else {
+            //栈顶括号(最右边的左括号,也就是最后一个进栈的括号)
+            const t = stack[stack.length - 1]
+            if (
+                (t === '[' && c === ']') ||
+                (t === '(' && c === ')') ||
+                (t === '{' && c === '}')
+            ) {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return stack.length === 0;//如果栈空了会返回true
+};
+//时间复杂度和空间复杂度都是O（n）
