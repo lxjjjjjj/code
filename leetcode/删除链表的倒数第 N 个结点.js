4507041fdf829b3c2fbbe7ce4faa4ab5908bff66
“lxjjjjjj” <1732749825@qq.com>
2022-11-12 14:28:12 +0800

    修改

---
+++ leetcode/删除链表的倒数第 N 个结点.js
@@ -0,0 +1,11 @@
+// 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+ var removeNthFromEnd = function(head, n) {
+    
+};
