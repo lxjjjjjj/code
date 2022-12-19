58301250dbcccde5bcf81b326f6485fe67ac21e6
“lxjjjjjj” <1732749825@qq.com>
2022-12-19 16:50:38 +0800

    修改

--- leetcode/最长连续序列.js
+++ leetcode/最长连续序列.js
@@ -34,3 +34,34 @@ var longestConsecutive = function(nums) {
 // console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
 // console.log(longestConsecutive([100,4,200,1,3,2]))
 console.log(longestConsecutive([1,2,0,1]))
+
+var longestConsecutive = function(nums) {
+    //思路：把 nums 存入哈希表，因为只要求值连续，
+    //      所以基于当前的数扩散去计数连续数值的哈希表长度就行了  
+    //1.先把数字都加进hash Set
+    //2.延伸左边的长度，同时计算删除
+    //3.再延伸右边的长度，同时计数删除
+    //由于每个数只能加入删除一次，时空都是O(n)
+    let hash = new Set(nums)
+    let res = 0
+    nums.forEach(curNum => {
+      let left = curNum
+      let right = curNum + 1
+      let max = 0
+      //数左边的hash
+      while(hash.has(left)){
+         max++
+         hash.delete(left)
+         left--;
+      } 
+      //数右边的hash
+      while(hash.has(right)){
+         max++
+         hash.delete(right)
+         right++;
+      }   
+      res = Math.max(res, max)
+    })
+    return res
+  };
+  
